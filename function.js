f0aa82e0754bd8220d8354700a7b588ffaded677
fittblitz <fittblitzlife@gmail.com>
2025-01-29 19:25:18 +0530

    Delete function.js

--- function.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function(){myFunction()};
-function myFunction(){
-  const nav = document.getElementById("headerid");
-  if(document.documentElement.scrollTop > 50){
-    nav.classList.add("scrolled");
-  }else{
-    nav.classList.remove("scrolled");
-  }
-}
